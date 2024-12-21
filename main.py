d785f3cf940cd313ec74e63f74f79e77a2e2e996
AstFreelancer <astfreelancer@mail.ru>
2024-12-21 21:37:23 +0400

    fix

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def contains_url(message: Message) -> bool:
 
 
 def contains_spam(message: Message) -> bool:
-    spam_words = ["10к в день", "Выплаты каждый день", "@best_trip_shop"]
+    spam_words = ["10к в день", "Выплаты каждый день", "Заработай бабок"]
     if message.text:
         for spam_word in spam_words:
             if spam_word in message.text:
